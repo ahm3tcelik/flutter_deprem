85f781793da1033f372e2c02ca7b859e2d95fd84
Ahmet ÇELİK <ahm3tcelik72@gmail.com>
2021-06-23 01:00:38 +0300

    initial commit

---
+++ lib/models/deprem.dart
@@ -0,0 +1,17 @@
+class Deprem {
+  final String baslik;
+  final String tarih;
+  final String saat;
+  final String buyukluk;
+  final double enlem;
+  final double boylam;
+
+  const Deprem({
+    required this.baslik,
+    required this.tarih,
+    required this.saat,
+    required this.buyukluk,
+    required this.enlem,
+    required this.boylam,
+  });
+}
